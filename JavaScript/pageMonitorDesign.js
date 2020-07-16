ceef5414a9ea95930e16b91890b0a6dc7e902799
KopeBalazs <balazskope@gmail.com>
2020-07-16 15:23:44 +0300

    Mukodik az ID

--- JavaScript/pageMonitorDesign.js
+++ JavaScript/pageMonitorDesign.js
@@ -1,17 +1,17 @@
 
 var menuMonitorElements;
 var monitorWindow;
-var monitorElements=[];
+var monitorElements = [];
 var monitorDesignMenu;
 
 main();
-function main(){
+function main() {
   init();
   addEventListenerToMenuElements();
 }
 
 //Structure to save created monitor elements and its attributes
-function monitorElement(elementID, elementType, elementName, elementSize, elementVerticalPosition, elementHorizontalPosition){
+function monitorElement(elementID, elementType, elementName, elementSize, elementVerticalPosition, elementHorizontalPosition) {
   this.elementID = elementID;
   this.elementType = elementType;
   this.elementName = elementName;
@@ -21,7 +21,7 @@ function monitorElement(elementID, elementType, elementName, elementSize, elemen
 }
 
 //Initiate frequently used and global variables
-function init(){
+function init() {
   menuMonitorElements = document.getElementsByClassName("monitorElement");
   monitorWindow = document.getElementById("monitorWindow");
   monitorDesignMenu = document.getElementById("monitorDesignMenu");
@@ -29,17 +29,17 @@ function init(){
 }
 
 //Add click event listener to menu elements
-function addEventListenerToMenuElements(){
-  for(var i=0; i<menuMonitorElements.length; i++){
+function addEventListenerToMenuElements() {
+  for (var i = 0; i < menuMonitorElements.length; i++) {
     menuMonitorElements[i].addEventListener("click", monitorMenuClick);
   }
   console.log("Event listeners added to menu elements");
 }
 
 //When the user clicks a menu element, place the new monitor element in the monitor window
-function monitorMenuClick(){
-  
+function monitorMenuClick() {
+
   //Append the container div to the monitor windows
-  monitorWindow.appendChild(createMonitorElementByType(this.id));
+  createMonitorElementByType(this.id);
 }
 
